1807974fe0eee72f8ddc00979842d2fd652bef0f
alce65 <alce65@hotmail.es>
2024-03-18 20:21:09 +0100

    Add initial components

---
+++ projects/challenge1/src/app/components/button-all/button-all.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isdi-button-all',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      button-all works!
+    </p>
+  `,
+  styles: ``
+})
+export class ButtonAllComponent {
+
+}
---
+++ projects/challenge1/src/app/components/gentleman/gentleman.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isdi-gentleman',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      gentelman works!
+    </p>
+  `,
+  styles: ``
+})
+export class GentlemanComponent {
+
+}
