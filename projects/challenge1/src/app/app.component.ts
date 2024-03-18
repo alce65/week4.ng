4a243ff01203e0abfa04b210b172559d357d3953
alce65 <alce65@hotmail.es>
2024-03-18 20:14:36 +0100

    Add initial app challenge1

---
+++ projects/challenge1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppGentsComponent } from './components/app-gents/app-gents.component';
+
+@Component({
+  selector: 'isdi-root',
+  standalone: true,
+  imports: [RouterOutlet, AppGentsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Angular Challenges';
+}
