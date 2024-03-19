ef8cfc1373cfe7fd0418b4930c6210a46127470e
alce65 <alce65@hotmail.es>
2024-03-19 13:31:02 +0100

    Add basic pages

--- projects/demo1/src/app/features/home/home.component.ts
+++ projects/demo1/src/app/features/home/home.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
 import { GreetingsComponent } from './components/greetings/greetings.component';
+import { CountersListComponent } from './components/counters-list/counters-list.component';
 
 @Component({
   selector: 'isdi-home',
   standalone: true,
-  imports: [GreetingsComponent],
+  imports: [GreetingsComponent, CountersListComponent],
   template: `
+    <p>App component</p>
+
+    <nav>
+      @for (page of pages; track $index) {
+        <button (click)="selectPage(page)">{{ page }}</button>
+      }
+    </nav>
+
+    <nav>
+      @for (page of pages; track $index) {
+        <button (click)="currentPage = page">{{ page }}</button>
+      }
+    </nav>
+
     <div>
       <h2>home</h2>
-      <p>Futura página home</p>
-      <isdi-greetings />
+      @if (currentPage === 'Greetings') {
+        <isdi-greetings />
+      } @else {
+        <isdi-counters-list />
+      }
     </div>
   `,
   styles: ``,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  pages: string[] = ['Greetings', 'Counter'];
+  currentPage: string = 'Greetings';
+
+  selectPage(page: string) {
+    this.currentPage = page;
+  }
+}
---
+++ projects/demo1/src/app/features/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isdi-todo',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      todo works!
+    </p>
+  `,
+  styles: ``
+})
+export class TodoComponent {
+
+}
