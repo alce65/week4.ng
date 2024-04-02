1264a6602da7699a85e1f66f9136fff5938ed033
alce65 <alce65@hotmail.es>
2024-04-02 16:56:42 +0200

    Add test with children event

--- projects/demo1/src/app/features/todo/components/todo-list/todo-list.component.spec.ts
+++ projects/demo1/src/app/features/todo/components/todo-list/todo-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoListComponent } from './todo-list.component';
 import { StorageService } from '../../../../core/services/storage.service';
+import { Task } from '../../../../core/models/task';
+import { By } from '@angular/platform-browser';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
   // jasmine.createSpy()
-  jasmine.createSpyObj(StorageService, [
-    {
-      getStorage: [],
-      setStorage: undefined,
-    },
-  ]);
+  const mockSrv = jasmine.createSpyObj(StorageService, {
+    getStorage: [],
+    setStorage: undefined,
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,15 +22,31 @@ describe('TodoListComponent', () => {
           provide: 'STORE_NAME',
           useValue: 'test',
         },
+        {
+          provide: StorageService,
+          useValue: mockSrv,
+        },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
+    component.tasks = [{ id: '1' } as Task];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('onLoad should set tasks', () => {
+    const mockTask = { id: '1' } as Task;
+    fixture.debugElement
+      .query(By.css('isdi-todo-card'))
+      .triggerEventHandler('updateEvent', mockTask);
+
+    // component.onUpdate(mockTask);
+    expect(component.storageSrv.getStorage).toHaveBeenCalled();
+    expect(mockSrv.getStorage).toHaveBeenCalled();
   });
 });
