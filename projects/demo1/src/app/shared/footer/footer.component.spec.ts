71f4639ff14d4380aafc096fea20d8cd76a533e7
alce65 <alce65@hotmail.es>
2024-04-06 20:22:08 +0200

    Update testing in core elements

--- projects/demo1/src/app/shared/footer/footer.component.spec.ts
+++ projects/demo1/src/app/shared/footer/footer.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,16 +8,21 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
+      imports: [FooterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    component.brand = 'Demo1';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the brand', () => {
+    const address = fixture.debugElement.query(By.css('address'));
+    expect(address.nativeElement.textContent).toContain('Demo1');
   });
 });
--- projects/demo1/src/app/shared/header/header.component.spec.ts
+++ projects/demo1/src/app/shared/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,16 +9,21 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
+      imports: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.title = 'Test Title';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title.nativeElement.textContent).toEqual('Test Title');
   });
 });
