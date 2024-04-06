b4ab518577a65e44c58d9a732dedcd604b39d98e
alce65 <alce65@hotmail.es>
2024-04-06 17:02:47 +0200

    Update testing in challenge 4

--- projects/challenges/src/app/challenge4/components/display/display.component.spec.ts
+++ projects/challenges/src/app/challenge4/components/display/display.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DisplayComponent } from './display.component';
+import { PhoneService } from '../../state/phone.service';
+import { By } from '@angular/platform-browser';
 
 describe('DisplayComponent', () => {
   let component: DisplayComponent;
   let fixture: ComponentFixture<DisplayComponent>;
+  let phoneSrv: PhoneService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DisplayComponent]
-    })
-    .compileComponents();
+      imports: [DisplayComponent],
+      providers: [PhoneService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DisplayComponent);
     component = fixture.componentInstance;
+    phoneSrv = TestBed.inject(PhoneService);
+    phoneSrv.setPhoneNumber('');
+    phoneSrv.setPhoneNumber('8');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the phone number', () => {
+    const number = fixture.debugElement.query(By.css('.number')).nativeElement;
+    expect(number.textContent).toBe('8');
   });
 });
